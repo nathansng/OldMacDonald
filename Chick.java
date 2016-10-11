976714007bdc199aa414aa1b348c7aad9f4670b2
nathansng <nathansng17@gmail.com>
2016-10-11 14:21:30 -0700

    Begin code

--- Chick.java
+++ Chick.java
@@ -1,4 +1,28 @@
-class Chick //implements Animal 
-{     
-     //your code here
+class Chick implements Animal {  
+
+	private String myType;
+	private String mySound;
+
+	public Chick (String type, String sound1, String sound2) {
+		myType = type;
+	
+	}
+
+	public Chick (String type, String sound) {
+		myType = type;
+		mySound = sound;
+	}
+
+	public Chick() {
+		myType = "unknown";
+		mySound  = "unknown";
+	}
+
+	public String getSound () {
+		return mySound;
+	}
+
+	public String getType () {
+		return myType;
+	}
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,8 @@
 
 void setup()
 {
-	//your variable declarations here
+	Farm myFarm = new Farm();
+
+	myFarm.animalSounds();
 }
 
